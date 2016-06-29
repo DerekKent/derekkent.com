993ba4ad511a48345551df5389169514e31816ab
Derek Kent <derek@derekkent.com>
2016-06-29 15:40:55 -0400

    Add Issues Page

---
+++ src/app/pages/issues/issues.js
@@ -0,0 +1,15 @@
+import {Controller} from 'superb';
+import {description as template} from './issues.html';
+
+export default class Issues extends Controller {
+
+    init() {
+        this.template = template;
+        this.css = '/app/pages/issues/issues.css';
+        this.view = {
+            tag: 'main',
+            classes: ['issues-page']
+        };
+    }
+
+}
