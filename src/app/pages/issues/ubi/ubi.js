a19699501e765a990652dac52538eb9018336932
Derek Kent <derek@derekkent.com>
2016-06-30 19:33:45 -0400

    Add Basic Income policy page

---
+++ src/app/pages/issues/ubi/ubi.js
@@ -0,0 +1,15 @@
+import {Controller} from 'superb';
+import {description as template} from './ubi.html';
+
+export default class BasicIncome extends Controller {
+
+    init() {
+        this.template = template;
+        this.css = '/app/pages/issues/ubi/ubi.css';
+        this.view = {
+            tag: 'main',
+            classes: ['issues_ubi-page', 'page']
+        };
+    }
+
+}
