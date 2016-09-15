10e00ac7e692eee5105a18835ecdbe532b4ca3f6
Derek Kent <derek@derekkent.com>
2016-09-14 23:29:54 -0400

    Cleanup JSPM dependencies

--- src/jspm.config.js
+++ src/jspm.config.js
@@ -38,25 +38,8 @@ SystemJS.config({
     "github:*/*.json"
   ],
   map: {
-    "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
-    "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
     "incremental-dom": "npm:incremental-dom@0.4.1",
-    "path": "github:jspm/nodelibs-path@0.2.0-alpha",
-    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
     "superb": "npm:superb.js@0.2.7"
   },
-  packages: {
-    "github:jspm/nodelibs-buffer@0.2.0-alpha": {
-      "map": {
-        "buffer-browserify": "npm:buffer@4.7.1"
-      }
-    },
-    "npm:buffer@4.7.1": {
-      "map": {
-        "base64-js": "npm:base64-js@1.1.2",
-        "ieee754": "npm:ieee754@1.1.6",
-        "isarray": "npm:isarray@1.0.0"
-      }
-    }
-  }
+  packages: {}
 });
