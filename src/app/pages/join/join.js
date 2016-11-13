29a8dda10d8270b75ad16ac898b8c1ccc37bafc4
Derek Kent <derek@derekkent.com>
2016-11-12 20:10:33 -0500

    Add form validation to Join page

--- src/app/pages/join/join.js
+++ src/app/pages/join/join.js
@@ -12,27 +12,55 @@ export default class Join extends Controller {
             classes: ['join-page', 'page']
         };
         this.model = {
-            joined: false
+            joined: false,
+            zipcode: '(\\d{5}([\\-]\\d{4})?)'
         };
+    }
+
+    @on('change form')
+    checkFormValidity(e) {
+        const form = e.delegateTarget;
+        const checkboxes = form.querySelectorAll('[type="checkbox"]');
+        const checked = Array.from(checkboxes).find((checkbox) => {
+            return checkbox.checked;
+        });
+
+        if (!checked) {
+            checkboxes[0].setCustomValidity('Don\'t forget to volunteer!');
+        } else {
+            checkboxes[0].setCustomValidity('');
+        }
     }
 
     @on('submit form')
     onSubscribeSubmit(e) {
         e.preventDefault();
 
-        const data = new FormData(e.delegateTarget);
+        const form = e.delegateTarget;
+        const data = new FormData(form);
 
-        fetch('https://derekkent.com/api/v1/join', {
-            method: 'POST',
-            body: data
-        }).then(() => {
-            this.model.submitting = false;
-            this.model.joined = true;
-            this.update();
-            window.scrollTo(0, 0);
-        });
+        if (!form.checkValidity()) {
+            this.model.error = 'Please enter your zip code and e-mail address, ' +
+                'and volunteer for one or more activities.';
+        } else {
+            this.model.error = false;
+            this.model.submitting = true;
 
-        this.model.submitting = true;
+            fetch('https://derekkent.com/api/v1/join', {
+                method: 'POST',
+                body: data
+            }).then(() => {
+                this.model.submitting = false;
+                this.model.joined = true;
+                this.update();
+                window.scrollTo(0, 0);
+            }).catch(() => {
+                this.model.submitting = false;
+                this.model.error = 'Oops, something went wrong.';
+                this.update();
+            });
+        }
+
         this.update();
     }
 
