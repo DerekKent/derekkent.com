6fad1b41d28c0ab36d31baf467c05c9ee13e8cf0
Derek Kent <derek@derekkent.com>
2016-12-23 13:21:46 -0600

    Add petition to ban fracking

---
+++ src/app/pages/petitions/fracking/fracking.js
@@ -0,0 +1,20 @@
+import Petition from '../petition';
+
+export default class FrackingPetition extends Petition {
+
+    setup() {
+        this.petition = {
+            conversionLabel: 'Gd7ACJr6z2wQiO3yowM',
+            title: 'Ban Fracking in Maryland',
+            lead: 'Help protect our health, climate and natural resources',
+            blurb: 'Fracking has been connected with cardiac and neurological ' +
+                'illnesses and premature births, pollutes drinking water, ' +
+                'poisons our air, contributes to climate change, and destroys ' +
+                'ecosystems.',
+            blurb2: 'Add your name to the petition to tell Maryland politicians ' +
+                'that you want to protect our health, climate, and natural ' +
+                'resources by keeping fracking out of our state.'
+        };
+    }
+
+}
