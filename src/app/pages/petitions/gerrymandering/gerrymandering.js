195cb8af157bfa23440bdabbda8e6c6000d1ae56
Derek Kent <derek@derekkent.com>
2016-12-24 18:10:22 -0600

    Add petition to end gerrymandering

---
+++ src/app/pages/petitions/gerrymandering/gerrymandering.js
@@ -0,0 +1,20 @@
+import Petition from '../petition';
+
+export default class GerrymanderingPetition extends Petition {
+
+    setup() {
+        this.petition = {
+            redirect: 'end_gerrymandering?amount=50.00&recurring=1',
+            conversionLabel: '_-uXCMW_52wQiO3yowM',
+            title: 'End Gerrymandering in Maryland',
+            lead: 'Help keep communities together',
+            blurb: 'Maryland has the most egregious gerrymandering in the ' +
+                'nation. It\'s time to end the embarrassment by taking redistricting ' +
+                ' away from politicians and giving it to a non-partisan organization.',
+            blurb2: 'Add your name to the petition to tell Maryland politicians ' +
+                'that voters should pick their politicians; politicians shouldn\'t ' +
+                'pick their voters.'
+        };
+    }
+
+}
