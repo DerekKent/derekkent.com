79449df779dceb265856ca7e7a4e2c245a2b8766
Derek Kent <derek@derekkent.com>
2017-01-13 17:25:27 -0500

    Add money in politics petition

---
+++ src/app/pages/petitions/corruption/corruption.js
@@ -0,0 +1,22 @@
+import Petition from '../petition';
+
+export default class CorruptionPetition extends Petition {
+
+    setup() {
+        this.petition = {
+            redirect: 'maryland-money?amount=50.00&recurring=1',
+            conversionLabel: 'VYivCPrYrW0QiO3yowM',
+            title: 'Get Big Money Out of Politics',
+            slug: 'money-in-politics',
+            lead: 'End the corrupting influence money has on our political system',
+            blurb: 'Campaigns should be reliant on building broad support among their constituents, ' +
+                'not on building narrow support among a small group of wealthy donors. ' +
+                'Yet, at $6000, the maximum individual donation limit in Maryland is among the ' +
+                'highest in the nation, inviting corruption.',
+            blurb2: 'Add your name to the petition to tell Maryland politicians ' +
+                'that you want to lower the maximum individual donation limit and ' +
+                'ban all corporate donations.'
+        };
+    }
+
+}
