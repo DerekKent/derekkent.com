2833954526c924fac7151cc6f14c68b1bce8234a
Derek Kent <derek@derekkent.com>
2017-01-15 21:04:54 -0500

    Add Feb 27 fracking deadline to petition

--- src/app/pages/petitions/fracking/fracking.js
+++ src/app/pages/petitions/fracking/fracking.js
@@ -13,7 +13,9 @@ export default class FrackingPetition extends Petition {
                 'illnesses and premature births, pollutes drinking water, ' +
                 'poisons our air, contributes to climate change, and destroys ' +
                 'ecosystems.',
-            blurb2: 'Add your name to the petition to tell Maryland politicians ' +
+            blurb2: 'But Maryland\'s fracking moratorium is coming to an ' +
+                'end on February 27 unless the General Assembly acts now.',
+            blurb3: 'Add your name to the petition to tell Maryland politicians ' +
                 'that you want to protect our health, climate, and natural ' +
                 'resources by keeping fracking out of our state.'
         };
