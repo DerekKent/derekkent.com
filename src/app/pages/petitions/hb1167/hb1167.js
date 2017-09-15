05f02f7ebb8cbe3be6fe0939b40652112a75ed19
Derek Kent <derek@derekkent.com>
2017-09-15 16:31:14 -0400

    Remove references to Pam Beidle

--- src/app/pages/petitions/hb1167/hb1167.js
+++ src/app/pages/petitions/hb1167/hb1167.js
@@ -13,8 +13,8 @@ export default class HB1167Petition extends Petition {
                 'care in Maryland. The bill would effectively ban most abortions — ' +
                 'even in cases of rape and incest — at a time when women\'s health ' +
                 'is also under attack from Congressional Republicans. ',
-            blurb2: 'Pam Beidle, Mark Chang, and Ted Sophocleus — the only Democrats ' +
-                'in the entire state who support this disastrous legislation — ' +
+            blurb2: 'Mark Chang, and Ted Sophocleus — two of only three Democrats ' +
+                'in the entire state to support this disastrous legislation — ' +
                 'should withdraw their support of it immediately.'
         };
     }
