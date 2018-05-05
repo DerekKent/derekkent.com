e2c05239144180d35511a4b95cfe3acf99fe3ede
Derek Kent <derek@derekkent.com>
2018-05-05 12:32:31 -0400

    Move colorized endorsements into color directory

--- src/app/pages/endorsements/endorsements.js
+++ src/app/pages/endorsements/endorsements.js
@@ -3,37 +3,37 @@ import template from './endorsements.html.js';
 
 const ENDORSEMENTS = [{
     name: '314 Action',
-    image: '/images/endorsements/314action.png',
+    image: '/images/endorsements/color/314action.png',
     link: 'http://www.314action.org/endorsed-candidates-1/'
 }, {
     name: 'Freethought Equality Fund',
-    image: '/images/endorsements/fef.png',
+    image: '/images/endorsements/color/fef.png',
     link: 'http://freethoughtequality.org/2018-endorsements-state-local/'
 }, {
     name: 'NARAL Pro-Choice Maryland PAC Endorsed Champion of Choice',
-    image: '/images/endorsements/naral.png',
+    image: '/images/endorsements/color/naral.png',
     link: 'https://maryland.prochoiceamericaaffiliates.org/wp-content/uploads/sites/11/2018/05/Endorsements.pdf'
 }, {
     name: 'Political Revolution',
-    image: '/images/endorsements/political-revolution.png',
+    image: '/images/endorsements/color/political-revolution.png',
     link: 'https://political-revolution.com/endorsements/'
 }, {
     name: 'Run for Something',
-    image: '/images/endorsements/run-for-something.png',
+    image: '/images/endorsements/color/run-for-something.png',
     link: 'https://runforsomething.net/candidates/derek-kent/'
 }];
 
 const RECOGNITIONS = [{
     name: 'Mom\'s Demand Action Gun Sense Candidate',
-    image: '/images/endorsements/gun-sense.png',
+    image: '/images/endorsements/color/gun-sense.png',
     link: ''
 }, {
     name: 'Represent Maryland Small Money Certified',
-    image: '/images/endorsements/represent.maryland.png',
+    image: '/images/endorsements/color/represent.maryland.png',
     link: ''
 }, {
     name: '#VoteProChoice Recommended',
-    image: '/images/endorsements/voteprochoice.png',
+    image: '/images/endorsements/color/voteprochoice.png',
     link: ''
 }];
 
